e3ae1a8643d86ca56e8349b528a24e7c94d61273
Silva-Gabriel <pottergryff8@gmail.com>
2021-11-05 12:12:45 -0300

    Valida usuário e adiciona autenticação e redirecionamento para outra página

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var email = document.getElementById('email');
+var usuario = document.getElementById('usuario');
 var senha = document.getElementById('senha');
 var olhoAberto = document.getElementById('eye');
 var olhoFechado = document.getElementById('eyeSlash');
@@ -40,16 +40,16 @@ function ocultarSenha()
 
 function borderInput()
 {
-    email.addEventListener('focus',() => {
-        email.style.borderColor = 'rgba(0,0,0,.95)'
+    usuario.addEventListener('focus',() => {
+        usuario.style.borderColor = 'royalblue'
     });
     
-    email.addEventListener('blur',() => {
-        email.style.borderColor = '#ccc';
+    usuario.addEventListener('blur',() => {
+        usuario.style.borderColor = '#ccc';
     });
     
     senha.addEventListener('focus',() => {
-        senha.style.borderColor = 'rgba(0,0,0,.95)'
+        senha.style.borderColor = 'royalblue'
     });
     
     senha.addEventListener('blur',() =>{
@@ -57,10 +57,53 @@ function borderInput()
     });
 }
 
-function cadastrar()
-{
-    
-}
+var entrar = document.getElementById('entrar');
+        entrar.addEventListener('click',() =>{
+            let msgSucess = document.getElementById('msgSucess');
+            let msgError = document.getElementById('msgError');
+            let form = document.querySelector('form');
+            let msgLogar = document.getElementById('msgLogar');
+
+            let listaUsuarios = [];
+
+            //Desestruturando objeto
+            let usuarioValido = {
+                nome: '',
+                usuario: '',
+                senha: ''
+            }
+
+            listaUsuarios = JSON.parse(localStorage.getItem('listaUsuarios'));
+            listaUsuarios.forEach(item => {
+                if(usuario.value == item.Usuario && senha.value == item.Senha)
+                {
+                    //Pega os valores do localStorage
+                    usuarioValido = {
+                        name: item.Nome,
+                        user: item.Usuario,
+                        key: item.Senha
+                    }
+                }
+            });
+
+            //Valida usuário e senha
+            if(usuario.value == usuarioValido.user && senha.value == usuarioValido.key){
+                msgSucess.style.display = 'block';
+                msgError.style.display = 'none';
+                msgLogar.style.display = 'flex';
+                msgSucess.innerHTML = '<strong>Autenticando</strong>';
+                form.style.maxHeight = '27rem';
+                setTimeout(() =>{
+                    location.href = 'home.html';
+                },3000)
+            }else{
+                msgError.style.display = 'block';
+                msgSucess.style.display = 'none';
+                msgError.innerHTML = '<strong>Usuário ou senha incorretos</strong>';
+                form.style.maxHeight = '26rem';
+                usuario.focus();
+            }
+        })
 
 //Inicializando as funções
 borderInput();
